e4b457dda0e8f1a7510214309647c74dacbc2032
Davidson <vader@assoweb.com.br>
2022-04-13 15:38:12 -0300

    Config initial structure

---
+++ src/components/Main/index.tsx
@@ -0,0 +1,13 @@
+import * as S from './styles'
+
+const Main = () => (
+  <S.Wrapper>
+    <S.Logo src="/img/react.png" alt="React"></S.Logo>
+    <S.Title>Boilerplate</S.Title>
+    <S.Description>
+      Typescript, ReactJS, NextJS and Styled Components
+    </S.Description>
+  </S.Wrapper>
+)
+
+export default Main
---
+++ src/components/Main/styles.ts
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  background-color: #06092b;
+  color: #fff;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  justify-content: center;
+  padding: 3rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Logo = styled.img`
+  width: 100%;
+  max-width: 150px;
+  margin: 0 0 20px;
+`
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+`
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`
